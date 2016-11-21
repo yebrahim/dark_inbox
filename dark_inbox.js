8bbd3ede961faa945b0543b55cc40169f431250f
Yasser Elsayed <yasser.ebrahim@gmail.com>
2016-11-21 01:28:12 -0800

    more style. search results need more styling

--- dark_inbox.js
+++ dark_inbox.js
@@ -3,7 +3,7 @@ var sg_head = document.getElementsByTagName('head')[0];
 var bg="#36393e"; // Main background
 var tb="#36393e"; // Header Background
 var sb="#2e3136"; // Sidebar background
-var tx="#696c6f"; // Sidebar text
+var tx="#cccccc"; // Sidebar text
 var sp="#686a6e"; // Separators
 var sl="#ffffff"; // Selected item text
 var hb="inherit"; // Title-box background
@@ -25,9 +25,13 @@ csscript.type = "text/css";
 csscript.innerHTML = ".c4,body{\
 	background-color:"+bg+"!important;\
 }\
-.b4 .bn, .bn, .bc, .u2.nJ{\
+.bn, .bc, .u2.nJ{\
   background-color:"+tb+"!important;\
   color:"+et+"!important;\
+}\
+.lQ, .ss, .qG{\
+  background-color:"+sb+"!important;\
+  color:"+tx+"!important;\
 }\
 .m9, .fX, .dy[disabled], .r6, .f0.jhBYqb{\
   color:"+et+"!important;\
@@ -35,13 +39,19 @@ csscript.innerHTML = ".c4,body{\
 .gN, .pE, .g-W-K, .l1, .gg, .KW, .dU, .du, .rA, .lJ, .qY, .jA, .cC{\
   filter:invert(100%);\
 }\
-.h8, .bB, .at, .X, .br>.bc, .aR, .k, .dm, .f5, .i8, .hs, .rI, .rJ, input, .de, .m7, .gb_ga, .gb_Bb, .gb_xb, .gb_ja{\
+.bB, .X, .br>.bc, .aR, .k, .dm, .f5, .i8, .hs, .rI, .rJ, .de, .m7, .gb_ga, .gb_Bb, .gb_xb, .gb_ja, .hA{\
   background-color:"+eb+"!important;\
   color:"+et+"!important;\
 }\
-.ac-P .bJ, .an, .nJ, .az{\
+.b4, .b4 .at, .h8, .ac-P .bJ, .an, .nJ, .az, .nE, input, .cN:hover, .cQ:hover, .gb_Cb:hover{\
   background-color:"+eheader+"!important;\
   color:"+et+"!important;\
+}\
+.b4 .at::before, bB .at::before, .at::before {\
+    box-shadow: none !important;\
+}\
+.HB {\
+  background-color:"+nb+"!important;\
 }\
 a.dj, a:visited.dj{\
 	color:"+el+"!important;\
