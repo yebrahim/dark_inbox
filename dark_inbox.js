b7de7f3e9f83544e720bef139b3b2856d6632aea
Yasser Elsayed <yasser.ebrahim@gmail.com>
2016-11-23 03:02:56 -0800

    fixed style for compose

--- dark_inbox.js
+++ dark_inbox.js
@@ -1,46 +1,57 @@
 var sg_head = document.getElementsByTagName('head')[0];
 
 var bg="#36393e"; // Main background
-var tb="#36393e"; // Header Background
+var tb="#202733"; // Header Background
 var sb="#2e3136"; // Sidebar background
-var tx="#aaaaaa"; // Sidebar text
+var tx="#eeeeee"; // Sidebar text
 var hb="inherit"; // Title-box background
 var eb="#222222"; // Email Background
-var eheader="#2d2d2d"; // Email Header
 var et="#999999"; // Email text
 var el="#0096cf"; // Email link
+var sp="#686a6e"; // Separators
+var eheader="#2d2d2d"; // Email Header
 var nbb="#245231"; // New badge background
-var nbf="black"; // New badge foreground
+var nbf="#cccccc"; // New badge foreground
 
 var csscript = document.createElement('style'); 
 csscript.type = "text/css"; 
 csscript.innerHTML = ".c4,body{\
-	background-color:"+bg+"!important;\
+  background-color:"+bg+"!important;\
 }\
-.bn, .bc, .u2.nJ{\
+.bn, .bc, .u2.nJ, .br .aV{\
   background-color:"+tb+"!important;\
   color:"+et+"!important;\
 }\
-.lQ, .ss, .qG{\
+.lQ, .ss{\
   background-color:"+sb+"!important;\
   color:"+tx+"!important;\
-  font-weight: normal !important;\
+  font-weight: bold !important;\
 }\
-.m9, .fX, .dy[disabled], .r6, .f0.jhBYqb{\
+.oa, .aO, .gb_X .gb_ea .gb_b, .gb_X .gb_vc.gb_wc{\
+  filter:inherit !important;\
+}\
+.qG, .qy, .gb_Xb{\
+  color:"+tx+"!important;\
+}\
+.m9, .dy[disabled], .r6, .f0.jhBYqb, #gb#gb a.gb_O{\
   color:"+et+"!important;\
 }\
-.gN, .pE, .g-W-K, .l1, .gg, .KW, .dU, .du, .rA, .lJ, .qY, .jA, .cC, .hX{\
-  filter:invert(100%);\
+.gL .dl, .g-W-aG-K, .IB, .ld, .gN, .nY, .t1, .pE, .g-W-K, .l1, .gg, .aO.Ipp0xc, .KW, .dU, .du, .rA, .lJ, .qY, .jA, .cC, .hX, .gX, .FK, .bD.SVcSYb, .gb_Xb, .e6 img, .actionIcon, .sR{\
+  filter:invert(100%) !important;\
+  box-shadow: none !important;\
 }\
-.bB, .X, .br>.bc, .aR, .k, .dm, .f5, .i8, .hs, .rI, .rJ, .de, .m7, .gb_ga, .gb_Bb, .gb_xb, .gb_ja, .hA{\
+.Y {\
+  border-bottom-color:"+sp+"!important;\
+}\
+.bB, .fX, .X, .gb_Eb *, .kH, .f9, .aR, .k, .bb .O-c3, .dm, .f5, .i8, .hs, .rI, .rJ, .de, .m7, .gb_ga, .gb_Bb, .gb_xb, .gb_ja, .hA, .d2, .fk, .do{\
   background-color:"+eb+"!important;\
   color:"+et+"!important;\
 }\
-.b4, .b4 .at, .X *, .h8, .ac-P .bJ, .an, .nJ, .az, .nE, input, .cN:hover, .cQ:hover, .gb_Cb:hover{\
-  background-color:"+eheader+"!important;\
+.b4, .b4 .at, .h8, .dK, .gb_Z:hover .gb_4, .ac-P .bJ, .an, .nJ, .az, .nE, .Nm, input, .cN:hover, .cQ:hover, .gb_Cb:hover, #aVMuZe:not(.m):not(.M) .ad>.I:not(.pU4C1e), #aVMuZe:not(.m):not(.M) .ay>.I, .ad.a1>*>*>.r:not(.B8), .ay.a1>*>*>.r, #aVMuZe:not(.m):not(.M) .ix.r{\
+  background:"+eheader+"!important;\
   color:"+et+"!important;\
 }\
-.b4 .at::before, bB .at::before, .at::before {\
+bB .at::before, .at::before, .aa::before, .an::before{\
     box-shadow: none !important;\
 }\
 .HB {\
