393b732b31c375b5fcc454cd10862c7a61bc7c1f
Yasser Elsayed <yasser.ebrahim@gmail.com>
2016-11-27 22:07:16 -0800

    fix email background colors, all dark

--- dark_inbox.js
+++ dark_inbox.js
@@ -1,7 +1,7 @@
 var sg_head = document.getElementsByTagName('head')[0];
 
 var bg="#36393e"; // Main background
-var tb="#202733"; // Header Background
+var tb="#202733"; // Header dark blue Background
 var sb="#2e3136"; // Sidebar background
 var tx="#eeeeee"; // Sidebar text
 var hb="inherit"; // Title-box background
@@ -20,7 +20,7 @@ csscript.innerHTML = "\
   background-color:"+bg+"!important;\
   color:"+et+"!important;\
 }\
-.bn, .bc, .u2.nJ, .br .aV, .X{\
+.bn, .bc, .u2.nJ, .br .aV, .X, .bJ, .ob{\
   background-color:"+tb+"!important;\
   color:"+et+"!important;\
 }\
@@ -38,18 +38,18 @@ csscript.innerHTML = "\
 .m9, .dy[disabled], .r6, .f0.jhBYqb, #gb#gb a.gb_O{\
   color:"+et+"!important;\
 }\
-.gL .dl, .g-W-aG-K, .IB, .ld, .gN, .nY, .t1, .g-W-K, .l1, .gg, .aO.Ipp0xc, .KW, .dU, .du, .rA, .lJ, .qY, .jA, .cC, .hX, .gX, .FK, .bD.SVcSYb, .gb_Xb, .e6 img, .tD img, .actionIcon, .sR, .g-Ku-mw .g-aq-jq, .pE>img{\
+.gL .dl, .g-W-aG-K, .IB, .ld, .gN, .nY, .t1, .g-W-K, .l1, .gg, .aO.Ipp0xc, .KW, .dU, .du, .rA, .lJ, .qY, .jA, .cC, .hX, .gX, .FK, .bD.SVcSYb, .gb_Xb, .e6 img, .tD img, .actionIcon, .sR, .g-Ku-mw .g-aq-jq, .pE>img, .hX>img{\
   filter:invert(100%) !important;\
   box-shadow: none !important;\
 }\
 .Y {\
   border-bottom-color:"+sp+"!important;\
 }\
-.bB, .fX, .ap, .ap :not(img), .gb_Eb *, .kH, .f9, .aR, .k, .bb .O-c3, .dm, .f5, .i8, .hs, .rI, .rJ, .de, .m7, .gb_ga, .gb_Bb, .gb_xb, .gb_ja, .hA, .d2, .fk, .do, .d2>.g-aq.g-Ku{\
+.bB, .pj, .iQ, .fX, .ap, .ap :not(img), .gb_Eb *, .kH, .f9, .aR, .k, .bb .O-c3, .dm, .f5, .i8, .hs, .rI, .rJ, .de, .m7, .gb_ga, .gb_Bb, .gb_xb, .gb_ja, .hA, .d2, .fk, .do, .d2>.g-aq.g-Ku, .gmail_msg *{\
   background-color:"+eb+"!important;\
   color:"+et+"!important;\
 }\
-.b4, .b4 .at, .h8, .dK, .gb_Z:hover .gb_4, .ac-P .bJ, .an, .nJ, .az, .nE, .Nm, .Mx, input, .cN:hover, .cQ:hover, .gb_Cb:hover, #aVMuZe:not(.m):not(.M) .ad>.I:not(.pU4C1e), #aVMuZe:not(.m):not(.M) .ay>.I, .ad.a1>*>*>.r:not(.B8), .ay.a1>*>*>.r, #aVMuZe:not(.m):not(.M) .ix.r, .d2>.g-aq.g-Ku:hover{\
+.b4, .b4 .at, .h8, .dK, .gb_Z:hover .gb_4, .an, .nJ, .az, .nE, .Nm, .Mx, input, .cN:hover, .cQ:hover, .gb_Cb:hover, #aVMuZe:not(.m):not(.M) .ad>.I:not(.pU4C1e), #aVMuZe:not(.m):not(.M) .ay>.I, .ad.a1>*>*>.r:not(.B8), .ay.a1>*>*>.r, #aVMuZe:not(.m):not(.M) .ix.r, .d2>.g-aq.g-Ku:hover{\
   background:"+eheader+"!important;\
   color:"+et+"!important;\
 }\
